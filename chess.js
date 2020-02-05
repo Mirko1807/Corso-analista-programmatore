573d1efc6d61d48e39f8d0f52a7c0791cfe951d6
Mirko1807 <59826817+Mirko1807@users.noreply.github.com>
2020-02-05 19:49:57 +0100

    Update chess.js

--- chess.js
+++ chess.js
@@ -1,9 +1,9 @@
 let size = 8;
 let black = "#"; //riga che comincia con il nero
-let white = "#"; //riga che comincia con il bianco
+let white = " "; //riga che comincia con il bianco
 for (let i=0;i<size-1;i++)
 {    //crea le prime 2 righe in base alla dim di size
-    if (black[i]==="#"&&white[i]===" ") 
+    if (black[i]==="#") 
     {
       black+=" "
       white+="#"
