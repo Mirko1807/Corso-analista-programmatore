e7ce5b76ee2fee691311f06c82903db70f88e85c
Mirko1807 <59826817+Mirko1807@users.noreply.github.com>
2020-02-06 10:40:24 +0100

    Update triangle.js

--- triangle.js
+++ triangle.js
@@ -1,7 +1,14 @@
 let lenght = 10;
 let base = "#";
-for (let i=0;i<lenght;i++)
-{
-  console.log(base);
-  base+="##"
+let space = " "
+let spacel = lenght
+for (let i = 0; i < lenght; i++) {
+  for (let j = 0; j < spacel; j++) {
+    space += " ";
+  }
+  spacel--;
+
+  console.log(space + base);
+  base += "##"
+  space = " ";
 }
